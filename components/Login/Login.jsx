11356ed94d859a979542f3abc821af87177f6a05
unknown <p.yeatun@gmail.com>
2021-06-03 11:51:06 -0400

    added some files

---
+++ components/Login/Login.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const Login = () => {
+    return (
+        <View>
+            <Text>Hello from the login</Text>
+        </View>
+    );
+};
+
+export default Login;
