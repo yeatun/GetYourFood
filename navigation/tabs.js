0224679c516caa780477ca59ceb2edbb22013c5b
Rahat47 <djrayhan8@gmail.com>
2021-06-03 15:56:58 +0600

    tabs file updated , Review component added

--- navigation/tabs.js
+++ navigation/tabs.js
@@ -7,6 +7,7 @@ import Products from '../components/Products/Products';
 import Orders from '../components/Orders/Orders';
 import Blog from '../components/Blog/Blog';
 import Auth from '../components/Auth/Auth';
+import Review from '../components/Review/Review';
 
 const Tab = createBottomTabNavigator();
 
@@ -27,6 +28,10 @@ function HomeTabs() {
                             iconName = focused ? "cart" : "cart-outline"
                             break;
 
+                        case "Reviews":
+                            iconName = focused ? "star" : "star-outline"
+                            break;
+
                         case "Orders":
                             iconName = focused ? "egg" : "egg-outline"
                             break;
@@ -40,7 +45,6 @@ function HomeTabs() {
                         default:
                             break;
                     }
-
                     return <Ionicons name={iconName} size={size} color={color} />;
                 }
             })}
@@ -52,8 +56,9 @@ function HomeTabs() {
             {loggedIn ?
                 <>
                     <Tab.Screen name="Home" component={Homepage} />
-                    <Tab.Screen name="Products" component={Products} />
+                    {/* <Tab.Screen name="Products" component={Products} /> */}
                     <Tab.Screen name="Orders" component={Orders} />
+                    <Tab.Screen name="Reviews" component={Review} />
                     <Tab.Screen name="Blogs" component={Blog} />
                 </>
                 :
