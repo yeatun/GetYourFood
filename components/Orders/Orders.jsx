2f718ed6e43d87f70ffea04aab70b487e0feefe8
Rahat47 <djrayhan8@gmail.com>
2021-06-04 00:35:33 +0600

    home page dummy data added and when click they redirec to orders page

--- components/Orders/Orders.jsx
+++ components/Orders/Orders.jsx
@@ -1,10 +1,38 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { Button, Text, View } from "react-native";
+import { getFoodByID } from "../../assets/data/data";
 
-const Orders = () => {
+const Orders = ({ route, navigation }) => {
+    const foodId = route?.params?.foodId;
+    const [food, setFood] = useState(null);
+
+    useEffect(() => {
+        setFood(getFoodByID(foodId));
+    }, [foodId]);
+
+    // food object is in the state
+
     return (
         <View>
-            <Text>Hello from the orders</Text>
+            {foodId ? (
+                <>
+                    {/* show food details here */}
+                    <Text>You are ordering food {foodId} </Text>
+                    <Text>You are ordering food {food.title} </Text>
+                    <Text>You are ordering food {food.description} </Text>
+                </>
+            ) : (
+                <Text>
+                    {/* if there is no food selected user will get this message and pressign the button will redirect to the HOme page 
+                    // TODO: style the button */}
+                    Please Select a food from{" "}
+                    <Button
+                        onPress={() => navigation.navigate("Home")}
+                        title="Home"
+                    />{" "}
+                    tab
+                </Text>
+            )}
         </View>
     );
 };
